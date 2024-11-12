d5466eb26bd9e91a81bb047d0dbb00258b6befaa
Viktor Jakovec <viktor.jakovec@student.gyarab.cz>
2024-11-12 14:01:29 +0100

    unsaved changes

--- karty.py
+++ karty.py
@@ -11,7 +11,6 @@ while True :
     num_array = np.random.randint(start_value, start_value + final_range, 99)
     num_array[0] = start_value
     num_array[length-1] = start_value + final_range
-    num_array[length/2] = final_median
     print (num_array)
     if (num_array.mean == final_mean and num_array.min == start_value and num_array.max == start_value+final_range and np.median(num_array) == final_median):
         break
