8de1c88d7f2dbddaa80fb1c25815d149cfc38fdd
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2025-01-27 20:20:11 +0100

    vinko

--- regCrime.py
+++ regCrime.py
@@ -4,30 +4,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
-
 communities_and_crime = fetch_ucirepo(id=183)
 
 X = communities_and_crime.data.features
 y = communities_and_crime.data.targets
-
 X = X.replace('?', np.nan)
 total_missing = X.isnull().sum().sum()
 
 X = X.dropna()
 y = y.loc[X.index]
 
-numeric_columns = X.select_dtypes(include=[np.number]).columns
-
-X_train, X_test, y_train, y_test = train_test_split(X[numeric_columns], y, test_size=0.3, random_state=42)
+numeric = X.select_dtypes(include=[np.number]).columns
+X_train, X_test, y_train, y_test = train_test_split(X[numeric], y, test_size=0.9, random_state=42)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
-
 y_pred = model.predict(X_test)
 
 print('Velikost trenovaci mnoziny: {}'.format(len(X_train)))
 print('Velikost testovaci mnoziny: {}'.format(len(X_test)))
 print('Pocet chybejicich hodnot: {}'.format(total_missing))
-
 print("MAE: {}".format(mean_absolute_error(y_test, y_pred)))
 print("RMSE: {}".format(mean_squared_error(y_test, y_pred)))
---
+++ regWine.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from ucimlrepo import fetch_ucirepo 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+wine_quality = fetch_ucirepo(id=186) 
+
+X = wine_quality.data.features 
+y = wine_quality.data.targets 
+total_missing = X.isnull().sum().sum()
+
+print(pd.DataFrame(wine_quality.data.features, columns=wine_quality.feature_names))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+print('Velikost trenovaci mnoziny: {}'.format(len(X_train)))
+print('Velikost testovaci mnoziny: {}'.format(len(X_test)))
+print('Pocet chybejicich hodnot: {}'.format(total_missing))
+print ("MAE: {}".format(mean_absolute_error(y_test, y_pred)))
+print ("RMSE: {}".format(mean_squared_error(y_test, y_pred)))
