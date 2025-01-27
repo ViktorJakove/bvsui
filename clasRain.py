6f98d77c95076e215ca50c842443cad612ffa50a
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2025-01-27 23:20:55 +0100

    hotovo

---
+++ clasRain.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+path_to_data = './weatherAUS.csv'
+data = pd.read_csv(path_to_data)
+
+data['Day'] = pd.to_datetime(data['Date'], errors='coerce').dt.day
+data['Month'] = pd.to_datetime(data['Date'], errors='coerce').dt.month
+data['Year'] = pd.to_datetime(data['Date'], errors='coerce').dt.year
+data.drop(columns=['Date'], inplace=True)
+
+data.fillna(value={'RainToday': 'No', 'RainTomorrow': 'No'}, inplace=True)
+
+input = ['Year', 'Month', 'Day', 'Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 
+         'Sunshine', 'WindGustDir', 'WindGustSpeed', 'WindDir9am', 
+         'WindDir3pm', 'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 
+         'Humidity3pm', 'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm', 
+         'Temp9am', 'Temp3pm', 'RainToday']
+output = ['RainTomorrow']
+X = data[input]
+y = data[output].values.ravel()
+categorical_cols = X.select_dtypes(include=['object']).columns.tolist()
+X = pd.get_dummies(X, columns=categorical_cols, drop_first=True)
+total_missing = X.isnull().sum().sum()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=50)
+print('Pocet objektu trenovaci casti: {}'.format(len(X_train)))
+print('Pocet objektu testovaci casti: {}'.format(len(X_test)))
+print('Pocet chybejicich hodnot: {}'.format(total_missing))
+
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_model.fit(X_train, y_train)
+rf_preds = rf_model.predict(X_test)
+print("Uspesnost nahodnych lesu:")
+print(classification_report(y_test, rf_preds))
--- regWine.py
+++ regWine.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from ucimlrepo import fetch_ucirepo 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.linear_model import LinearRegression
 wine_quality = fetch_ucirepo(id=186) 
 
 X = wine_quality.data.features 
@@ -16,8 +16,8 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
-print('Velikost trenovaci mnoziny: {}'.format(len(X_train)))
-print('Velikost testovaci mnoziny: {}'.format(len(X_test)))
+print('Pocet objektu trenovaci mnoziny: {}'.format(len(X_train)))
+print('Pocet objektu testovaci mnoziny: {}'.format(len(X_test)))
 print('Pocet chybejicich hodnot: {}'.format(total_missing))
 print ("MAE: {}".format(mean_absolute_error(y_test, y_pred)))
 print ("RMSE: {}".format(mean_squared_error(y_test, y_pred)))
